117881e62703eff9b02ac316fdeab1bbbeee7635
Romain Menke <romainmenke@me.com>
2015-07-14 18:43:36 +0200

    Initial Commit

---
+++ Fallout_Hacker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fallout_Hacker
+//
+//  Created by Romain Menke on 14/07/15.
+//  Copyright © 2015 menke dev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
